303d1630402c7722631f995da12b68fba035dc85
Austin Hou <austhou@gmail.com>
2018-08-12 20:47:01 -0700

    made more responsive

--- Spoken.js
+++ Spoken.js
@@ -15,10 +15,10 @@ exec('starAnimate', 'show');
 
 window.onload = function() {
     //quoteFade
-    exec('quote', 'fade', 1500);
-    exec('leave', 'fade', 1500);
+    exec('quote', 'fade', 1000);
+    exec('leave', 'fade', 1000);
     //galaxiaFade
-    exec('galaxia', 'fade', 200);
+    exec('galaxia', 'fade', 100);
 }
 
 //leave a name transition
@@ -27,13 +27,13 @@ document.getElementById('leave').onclick = function () {
     exec('quote', 'fade');
     exec('leave', 'fade');
     //killQuote
-    exec('quote', 'disp', 1500);
-    exec('leave', 'disp', 1500);
+    exec('quote', 'disp', 1000);
+    exec('leave', 'disp', 1000);
     //spawnForm
-    exec('form', 'show', 1500);
+    exec('form', 'show', 1000);
     //formFade
-    exec('form', 'fade', 1550);
-    setTimeout(focusText, 1600);
+    exec('form', 'fade', 1050);
+    setTimeout(focusText, 1100);
 }
 
 //submit name transition
@@ -45,13 +45,13 @@ var submitName = function() {
     //star fade
     exec('starAnimate', 'fade');
     //galaxia fade
-    exec('galaxia', 'fade', 1500);
+    exec('galaxia', 'fade', 1000);
     //star rise
-    exec('starAnimate', 'rise', 1550);
+    exec('starAnimate', 'rise', 1050);
     //seeyou show
-    exec('seeyou', 'show', 3000);
+    exec('seeyou', 'show', 2000);
     //seeyou fade
-    exec('seeyou', 'fade', 3050);
+    exec('seeyou', 'fade', 2050);
     return false;
 }
 
@@ -62,23 +62,23 @@ document.getElementById('reset').onclick = function () {
     //seeyou fade
     exec('seeyou', 'fade');
     //seeyou show
-    exec('seeyou', 'show', 1500);
+    exec('seeyou', 'show', 1000);
     //galaxia fade
-    exec('galaxia', 'fade', 1500);
+    exec('galaxia', 'fade', 1000);
     //kill quote
-    exec('quote', 'disp', 3000);
-    exec('leave', 'disp', 3000);
+    exec('quote', 'disp', 2000);
+    exec('leave', 'disp', 2000);
     //fade quote
-    exec('quote', 'fade', 3050);
-    exec('leave', 'fade', 3050);
+    exec('quote', 'fade', 2050);
+    exec('leave', 'fade', 2050);
     //spawn form
-    exec('form', 'show', 3100);
+    exec('form', 'show', 2100);
     //fade form
-    exec('form', 'fade', 3150);
+    exec('form', 'fade', 2150);
     //fade formContent
-    exec('formContent', 'fade', 3200);
+    exec('formContent', 'fade', 2200);
     //star rise
-    exec('starAnimate', 'rise', 4050);
+    exec('starAnimate', 'rise', 2550);
     //starLayer show
-    exec('starAnimate', 'show', 4050);
+    exec('starAnimate', 'show', 2550);
 }
